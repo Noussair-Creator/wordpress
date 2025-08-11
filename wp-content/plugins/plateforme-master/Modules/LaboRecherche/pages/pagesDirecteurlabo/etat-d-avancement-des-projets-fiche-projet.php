eeca99ebae483affae09364080a06766c0f07915
Noussair <noussairmekni7@gmail.com>
2025-08-11 14:34:20 +0100

    fini dericteur labo

--- wp-content/plugins/plateforme-master/Modules/LaboRecherche/pages/pagesDirecteurlabo/etat-d-avancement-des-projets-fiche-projet.php
+++ wp-content/plugins/plateforme-master/Modules/LaboRecherche/pages/pagesDirecteurlabo/etat-d-avancement-des-projets-fiche-projet.php
@@ -10,7 +10,7 @@ require_once plugin_dir_path(__FILE__) . '../requireApi.php';
 
 <head>
     <meta charset="UTF-8">
-    <title>FICHE PROJET</title>
+    <title>Fiche Projet</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
